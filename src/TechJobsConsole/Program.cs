4430a37268a9f684b42369d231bae309e779ff2f
monsieurverde <domverdemgmt@gmail.com>
2019-11-12 16:31:44 -0500

    minor updates

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -129,7 +129,7 @@ namespace TechJobsConsole
                         Console.WriteLine(key + (": " + job[key]));
                     }
 
-                    Console.WriteLine("***");
+                    Console.WriteLine("***\n");
                 }
 
             }
